d5d7d1340c828c3e11185c2f2eb9b41b0b0fa78f
Jblack45-max <97457377+Jblack45-max@users.noreply.github.com>
2022-09-10 11:32:16 -0600

    Add files via upload

--- Testing/main.js
+++ Testing/main.js
@@ -22,9 +22,12 @@ let pole1;
 let pole2;
 let percent;
 let max_val;
+let remain1;
+let remain2;
+let heatPrint1;
+let maxAmp;
+let get = "document.getElementById";
 const heatAcArray = [0];
-
-
 
 function submitClicked() {
 
@@ -49,6 +52,9 @@ function submitClicked() {
     let otherUserPicked5 = document.getElementById("otherList5").value;
     let otherBreaker5 = parseInt(document.getElementById("otherInput5").value);
     let otherLine5;
+    //PRINTING//
+
+
     //Variable name declared for final calculation/
     userPickNum = document.getElementById("heatListNum").value;
     //ONE
@@ -155,6 +161,7 @@ function submitClicked() {
 
     //Calulating 120% Rule - Max Solar Allowance//
     maxSolar = mspRating * 1.20 - mainRating;
+    maxAmp = mspRating * 1.20;
     document.getElementById("solarAllowed").innerHTML = maxSolar + "A";
 
     //Lighting//
@@ -172,6 +179,7 @@ function submitClicked() {
 
 
     heatTotal1 = (heatInput1 * .8 * pole1) * percent;
+    heatPrint1 = heatInput1 * .8 * pole1;
     heatAcArray.push(heatTotal1);
 
     heatTotal2 = heatInput2 * .8 * pole2;
@@ -184,13 +192,98 @@ function submitClicked() {
 
     //Final Calculation
     finalTotal = (((mainTotals - 10000) * .4 + 10000) + finalMath) / 240;
+    remain1 = mainTotals - 10000;
+    remain2 = (mainTotals - 10000) * .4;
 
     rounded = Math.round(finalTotal);
     ampA = mainRating - rounded;
 
     document.getElementById("ampsUsed").innerHTML = Math.round(finalTotal) + "A";
     document.getElementById("ampsAvailable").innerHTML = ampA + "A";
+    localStorage.setItem('0001', finalTotal);
+
+    // THIS IS FOR THE PRINT FORM
+    if (userPickNum == "one") {
+        document.getElementById("print01").innerHTML = heatPrint1;
+        document.getElementById("print01.1").innerHTML = heatTotal1;
+        document.getElementById("print02").innerHTML = 0;
+        document.getElementById("print02.1").innerHTML = 0;
+    } else if (userPickNum == "two") {
+        document.getElementById("print01").innerHTML = 0;
+        document.getElementById("print01.1").innerHTML = 0;
+        document.getElementById("print02").innerHTML = heatPrint1;
+        document.getElementById("print02.1").innerHTML = heatTotal1;
+    } else {
+        document.getElementById("print01").innerHTML = 0;
+        document.getElementById("print01.1").innerHTML = 0;
+        document.getElementById("print02").innerHTML = 0;
+        document.getElementById("print02.1").innerHTML = 0;
+    }
 
 
+    document.getElementById("print03").innerHTML = heatTotal2;
+    document.getElementById("print03.1").innerHTML = heatTotal2;
+    document.getElementById("print04").innerHTML = finalMath;
+    document.getElementById("print05").innerHTML = squareFoot;
+    document.getElementById("print05.1").innerHTML = lightsTotal;
+    document.getElementById("print06").innerHTML = smallInput;
+    document.getElementById("print06.1").innerHTML = smallTotal01;
+    document.getElementById("print08.1").innerHTML = otherLine1;
+    document.getElementById("print09.1").innerHTML = otherLine2;
+    document.getElementById("print10.1").innerHTML = otherLine3;
+    document.getElementById("print11.1").innerHTML = otherLine4;
+    document.getElementById("print12.1").innerHTML = otherLine5;
+    document.getElementById("print13").innerHTML = otherTotal;
+    document.getElementById("print14").innerHTML = remain1;
+    document.getElementById("print14.1").innerHTML = remain2;
+    document.getElementById("print15").innerHTML = remain2;
+    document.getElementById("print16").innerHTML = remain2;
+    document.getElementById("print16.1").innerHTML = finalTotal;
+    document.getElementById("print17").innerHTML = mspRating;
+    document.getElementById("print17.1").innerHTML = maxAmp;
+    document.getElementById("print18").innerHTML = maxAmp;
+    document.getElementById("print18.1").innerHTML = mainRating;
+    document.getElementById("print18.2").innerHTML = maxSolar;
+    document.getElementById("print19").innerHTML = maxSolar;
 
+}
+
+
+function printClicked() {
+    document.getElementById("printing").style.display = "block";
+    document.getElementById("calculator").style.display = "none";
+    document.body.style.backgroundColor = 'white';
+}
+
+function backClicked() {
+    document.getElementById("printing").style.display = "none";
+    document.getElementById("calculator").style.display = "block";
+    document.body.style.backgroundColor = '#2c2c2c';
+}
+
+
+var img = document.getElementById("image");
+var img1 = document.getElementById("image1");
+
+function setImage() {
+    const files = document.getElementById("chooseFile").files;
+    if (!files || files.length == 0)
+        return;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        img.src = reader.result;
+    };
+}
+function setImage1() {
+    const files = document.getElementById("chooseFile1").files;
+    if (!files || files.length == 0)
+        return;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        img1.src = reader.result;
+    };
 }
